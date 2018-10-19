be8803fe9d5fe144adfbf00516d06c058931fe70
Felix <felix330@gmail.com>
2018-10-19 19:58:19 +0200

    Upgrade to .NET 4.0, Serial Connection

--- Assets/VRScientificToolkit/Serial Connection/STKReceiveSerial.cs
+++ Assets/VRScientificToolkit/Serial Connection/STKReceiveSerial.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
+using Unity.Jobs;
 
 public class STKReceiveSerial : MonoBehaviour {
 
@@ -10,15 +11,52 @@ public class STKReceiveSerial : MonoBehaviour {
 
     private SerialPort stream;
     private string currentValue;
-	// Use this for initialization
-	void Start () {
+    
+    // Use this for initialization
+    void Start () {
         stream = new SerialPort(port, baudRate);
+        stream.DataReceived += new SerialDataReceivedEventHandler(OnDataReceived);
+        
         stream.Open();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        currentValue = stream.ReadLine();
-        Debug.Log(currentValue);
-	}
+        if (stream.BytesToRead != 0)
+        {
+            Debug.Log(stream.ReadLine());
+        }
+        /*Debug.Log(stream.BytesToRead);
+        try
+        {
+            //Debug.Log(stream.ReadLine());
+        }
+        catch (System.Exception e)
+        {
+
+        }*/
+    }
+
+    private void OnDataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        Debug.Log("Got some data");
+    }
+
+    /*public struct ReadSerial : IJob
+    {
+        public SerialPort stream;
+        public float result;
+
+        public void Execute()
+        {
+            stream = new SerialPort("COM4", 9600);
+            stream.Open();
+            Debug.Log(stream.ReadLine());
+            ReadSerial reader = new ReadSerial();
+            reader.stream = stream;
+            JobHandle handle = reader.Schedule();
+            handle.Complete();
+        }
+    }*/
 }
+
