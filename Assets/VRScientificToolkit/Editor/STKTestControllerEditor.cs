90b22a15cccc7b98923d6d17c4cd6359bf8b9da5
Felix <felix330@gmail.com>
2018-10-31 20:54:48 +0100

    Adding Properties in Editor, first version of test UI

---
+++ Assets/VRScientificToolkit/Editor/STKTestControllerEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(STKTestController))]
+public class STKTestControllerEditor : Editor
+{
+    private string newName;
+
+    public override void OnInspectorGUI()
+    {
+        STKTestController myTarget = (STKTestController)target;
+        newName = EditorGUILayout.TextField("Name of new Property: ",newName);
+
+        if (newName != null && newName != "")
+        {
+            if (GUILayout.Button("Add Property"))
+            {
+                myTarget.AddProperty(newName);
+            }
+        } else
+        {
+            GUILayout.Label("Please choose a name before adding a new property.");
+        }
+    }
+}
--- Assets/VRScientificToolkit/Event System/STKTestController.cs
+++ Assets/VRScientificToolkit/Event System/STKTestController.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class STKTestController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject verticalGroup; //Parent object for spawned selections
+    public GameObject propertyPrefab;
+    public GameObject startButton;
+
+    public int numberOfProperties;
+    public bool addProperty;
+
+    private Hashtable properties = new Hashtable();
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -13,4 +22,15 @@ public class STKTestController : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void AddProperty(string name)
+    {
+        Debug.Log("add property");
+        GameObject newProperty = GameObject.Instantiate(propertyPrefab);
+        newProperty.transform.parent = verticalGroup.transform;
+        newProperty.GetComponent<STKTestControllerProperty>().text.text = name;
+        properties.Add(name, newProperty);
+        startButton.transform.parent = transform;
+        startButton.transform.parent = verticalGroup.transform; //Reset button to last position
+    }
 }
