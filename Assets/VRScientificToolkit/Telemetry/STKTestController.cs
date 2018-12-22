54bd49bdb2d591ff7f9f89f41abf58c6ee1debd9
Felix <felix330@gmail.com>
2018-12-22 16:09:33 +0100

    Change selection to stage when spawning

--- Assets/VRScientificToolkit/Telemetry/STKTestController.cs
+++ Assets/VRScientificToolkit/Telemetry/STKTestController.cs
@@ -50,6 +50,7 @@ public class STKTestController : MonoBehaviour {
         GameObject newStage = Instantiate(stagePrefab);
         newStage.transform.parent = verticalGroup.transform;
         newStage.GetComponent<STKTestStage>().myController = gameObject.GetComponent<STKTestController>();
+        Selection.activeGameObject = newStage;
         testStages = Array.ConvertAll(STKArrayTools.AddElement(newStage,testStages), item => item as GameObject);
         testStages = Array.ConvertAll(STKArrayTools.ClearNullReferences(testStages), item => item as GameObject);
         foreach (GameObject g in testStages)
