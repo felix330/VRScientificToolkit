8984aa7a8bf55c1c3f1d458e6eacd34ccaea19d7
Felix <felix330@gmail.com>
2018-12-31 13:25:37 +0100

    Can Create Heatmaps in R

--- RTools/CreateHeatmap.R
+++ RTools/CreateHeatmap.R
@@ -1,4 +1,9 @@
-CreateHeatmap <- function (eventName,axis)
+CreateHeatmap <- function ()
 {
-  
+  dataPoints = otherJSONData$Stage0$Camera5952$position_Transform;
+  require(MASS)
+  unlisted <- matrix(unlist(test), ncol = 3, byrow = TRUE);
+  dens <- kde2d(unlisted[,1], unlisted[,3], n=100);
+  filled.contour(dens)
+  #pointsMatrix = data.matrix(dataPoints);
 }
